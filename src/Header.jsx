c248c7cfe1cf92b67daccdf5a7f291d6c754c527
Rajdev-ullash <rajdevullash111@gmail.com>
2024-01-27 01:14:08 +0600

    [Done] movielist moviewcard rating footer component

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header>
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
-          <img src={Logo} width="139" height="26" alt="" />
+          <img src={Logo} width="139" height="26" alt="logo" />
         </a>
 
         <ul className="flex items-center space-x-5">
@@ -17,7 +17,7 @@ export default function Header() {
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
-              <img src={Ring} width="24" height="24" alt="" />
+              <img src={Ring} width="24" height="24" alt="ring" />
             </a>
           </li>
           <li>
@@ -25,7 +25,7 @@ export default function Header() {
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
-              <img src={Moon} width="24" height="24" alt="" />
+              <img src={Moon} width="24" height="24" alt="moon" />
             </a>
           </li>
           <li>
---
+++ src/cine/Rating.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Star from "../assets/star.svg";
+export default function Rating({ value }) {
+  const starts = Array(value).fill(Star);
+  return (
+    <>
+      {starts.map((star, index) => (
+        <img key={index} src={star} width="14" height="14" alt="" />
+      ))}
+    </>
+  );
+}
